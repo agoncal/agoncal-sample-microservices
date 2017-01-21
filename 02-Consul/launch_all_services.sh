2151234f55d34d976e459957f4c783e689c9829c
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-01-21 16:51:40 +0100

    refactoring names

--- 02-Consul/launch_all_services.sh
+++ 02-Consul/launch_all_services.sh
@@ -2,9 +2,9 @@
 
 # You need first to package all the services with Wildfly Swarm with mvn clean package -Pswarm
 
-java -jar ./service1/target/service1-swarm.jar &
-java -jar ./service2/target/service2-swarm.jar &
-#java -jar ./service3/target/service3-swarm.jar &
-#java -jar ./service4/target/service4-swarm.jar &
-#java -jar ./service5/target/service5-swarm.jar &
-#java -jar ./service6/target/service6-swarm.jar &
+java -jar ./quote1/target/quote1-swarm.jar &
+java -jar ./quote2/target/quote2-swarm.jar &
+#java -jar ./quote3/target/quote3-swarm.jar &
+#java -jar ./quote4/target/quote4-swarm.jar &
+#java -jar ./quote5/target/quote5-swarm.jar &
+#java -jar ./quote6/target/quote6-swarm.jar &
